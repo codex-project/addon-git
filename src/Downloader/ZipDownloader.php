b830f7034671e96892221215ef4bd5baa17ca181
Robin Radic <robin@radic.nl>
2016-06-19 13:22:53 +0200

    CODEX-12 #done klaar

--- src/Downloader/ZipDownloader.php
+++ src/Downloader/ZipDownloader.php
@@ -19,37 +19,29 @@ class ZipDownloader extends AbstractDownloader
 
     protected $path;
 
+    protected $owner, $repo, $ref, $tmpPath;
+
     public function download($owner, $repo, $ref)
     {
+        // Here we download and extract the zip file into a temporary location
+        // The extracted files will then be copied to the documentation's ref dir
+        // after which the extracted stuff will be cleaned up.
 
-        if ( null === self::$hasSystemUnzip )
-        {
-            $finder               = new ExecutableFinder;
-            self::$hasSystemUnzip = (bool)$finder->find('unzip');
-        }
-        if ( !class_exists('ZipArchive') && !self::$hasSystemUnzip )
-        {
-            // php.ini path is added to the error message to help users find the correct file
-            $iniPath = php_ini_loaded_file();
-            if ( $iniPath )
-            {
-                $iniMessage = 'The php.ini used by your command-line PHP is: ' . $iniPath;
-            }
-            else
-            {
-                $iniMessage = 'A php.ini file does not exist. You will have to create one.';
-            }
-            $error = "The zip extension and unzip command are both missing, skipping.\n" . $iniMessage;
-            throw new \RuntimeException($error);
-        }
+        // Check up on method to zip
+        $this->hasUnzipCapabilities();
 
-        $fs      = $this->getFs();
-        $pfs     = $this->project->getFiles();
+        $this->owner = $owner;
+        $this->repo = $repo;
+        $this->ref = $ref;
+
+        $fs  = $this->getFs();
+        $pfs = $this->project->getFiles();
 
 
         // prepare new temporary directory
         $tmpPath = storage_path("codex/git/{$owner}.{$repo}.{$ref}");
-        if($fs->isDirectory($tmpPath)){
+        if ( $fs->isDirectory($tmpPath) )
+        {
             $fs->deleteDirectory($tmpPath);
         }
         $fs->ensureDirectory($tmpPath);
@@ -59,8 +51,18 @@ class ZipDownloader extends AbstractDownloader
         $zip->open($this->remote->downloadArchive($repo, $ref, "{$tmpPath}.zip", $owner));
         $zip->extractTo($tmpPath);
 
-        $tmpExtractedPath = $fs->globule($glob = path_join($tmpPath, '*'))[0];
-        $files = collect($fs->allFiles(path_join($tmpExtractedPath, $this->docPath)))
+        $tmpExtractedPath = $fs->globule($glob = path_join($tmpPath, '*'))[ 0 ];
+
+        if ( $fs->exists($tmpIndexPath = path_join($tmpExtractedPath, $this->indexPath)) )
+        {
+            $pfs->put(path_join($ref, 'index.md'), $fs->get($tmpIndexPath));
+        }
+        if ( $fs->exists($tmpMenuPath = path_join($tmpExtractedPath, $this->menuPath)) )
+        {
+            $pfs->put(path_join($ref, 'menu.yml'), $fs->get($tmpMenuPath));
+        }
+
+        $files            = collect($fs->allFiles(path_join($tmpExtractedPath, $this->docPath)))
             ->transform(function ($item)
             {
                 return (string)$item;
@@ -76,9 +78,8 @@ class ZipDownloader extends AbstractDownloader
             $this->getSyncer()->fire('tick.file', [ $current, $total, $destPath, $this->syncer, $this ]);
         }
 
-        $pfs->put(path_join($ref, 'index.md'), $fs->get(path_join($tmpExtractedPath, $this->indexPath)));
-        $pfs->put(path_join($ref, 'menu.yml'), $fs->get(path_join($tmpExtractedPath, $this->menuPath)));
-
+        $pfs->put(path_join($ref, 'index.md'), $fs->get($tmpIndexPath));
+        $pfs->put(path_join($ref, 'menu.yml'), $fs->get($tmpMenuPath));
         $fs->deleteDirectory($tmpPath);
         $fs->delete("{$tmpPath}.zip");
         return $this;
@@ -89,5 +90,29 @@ class ZipDownloader extends AbstractDownloader
      */
     public function moveTo($dirPath)
     {
+    }
+
+    protected function hasUnzipCapabilities()
+    {
+        if ( null === self::$hasSystemUnzip )
+        {
+            $finder               = new ExecutableFinder;
+            self::$hasSystemUnzip = (bool)$finder->find('unzip');
+        }
+        if ( ! class_exists('ZipArchive') && ! self::$hasSystemUnzip )
+        {
+            // php.ini path is added to the error message to help users find the correct file
+            $iniPath = php_ini_loaded_file();
+            if ( $iniPath )
+            {
+                $iniMessage = 'The php.ini used by your command-line PHP is: ' . $iniPath;
+            }
+            else
+            {
+                $iniMessage = 'A php.ini file does not exist. You will have to create one.';
+            }
+            $error = "The zip extension and unzip command are both missing, skipping.\n" . $iniMessage;
+            throw new \RuntimeException($error);
+        }
     }
 }
