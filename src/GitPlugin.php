245d7a0331b69ff3d0db1e0dd63d7850e61b2c2e
Robin Radic <robin@radic.nl>
2016-09-03 13:53:43 +0200

    Plugins taking shape

---
+++ src/GitPlugin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Part of the Codex Project packages.
+ *
+ * License and copyright information bundled with this package in the LICENSE file.
+ *
+ * @author Robin Radic
+ * @copyright Copyright 2016 (c) Codex Project
+ * @license http://codex-project.ninja/license The MIT License
+ */
+namespace Codex\Addon\Git;
+
+use Codex\Addons\Annotations\Plugin;
+use Codex\Addons\BasePlugin;
+
+/**
+ * This is the class GitPlugin.
+ *
+ * @package        Codex\Addon
+ * @author         Radic
+ * @copyright      Copyright (c) 2015, Radic. All rights reserved
+ *
+ * @Plugin("git")
+ */
+class GitPlugin extends BasePlugin
+{
+
+}
